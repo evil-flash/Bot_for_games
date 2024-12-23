142566c189a62c66dd727db97c8e9012ae66fc86
evil_FlasH <evil.flash@yandex.ru>
2024-12-23 15:35:49 +0300

    add start tg and bums

--- bot_for_games.py
+++ bot_for_games.py
@@ -1,1 +1,24 @@
 # !Python3
+# !Автокликер для игр в telegram
+
+import subprocess, time, pyautogui
+
+program_path = "C:\Program Files\BlueStacks_nxt\HD-Player.exe"
+telegram_coordinate = 950, 500
+search_coordinate = 935, 250
+write_Bums = 'Bums'
+bums_coordinate = 910, 430
+
+subprocess.Popen(program_path)
+time.sleep(10)
+
+# запускаем telegram
+pyautogui.click(telegram_coordinate)
+time.sleep(15)
+
+# выбираем поиск
+pyautogui.click(search_coordinate)
+
+# запускаем Bums
+pyautogui.typewrite(write_Bums)
+pyautogui.click(bums_coordinate)
